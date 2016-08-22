c48c4dcb46cde3f8ccd273f15320d361afeb9fe1
zhangjian3364 <1549172463@qq.com>
2016-08-22 12:33:03 +0800

    攻略页面的增加

---
+++ Pet/src/com/example/pet/GonglueActivity.java
@@ -0,0 +1,76 @@
+package com.example.pet;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+public class GonglueActivity extends Activity {
+	ListView listView;
+	SimpleAdapter simpleadapter;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_gonglue);
+		listView = (ListView) findViewById(R.id.listview);
+		getdata();
+		String[] from = { "imag", "title1", "title2", "title3", "imagJt" };
+		int[] to = { R.id.imageview, R.id.textview1, R.id.textview2,
+				R.id.textview3, R.id.imageviewJt };
+		simpleadapter = new SimpleAdapter(GonglueActivity.this, getdata(),
+				R.layout.activity_simpleadapter_gonglue, from, to);
+		listView.setAdapter(simpleadapter);
+	}
+
+	public List<? extends Map<String, ?>> getdata() {
+		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("imag", R.drawable.dog1);
+		map.put("title1", "金毛寻回犬");
+		map.put("title2", "Canis lupus familiaris");
+		map.put("title3", "夏天是金毛犬最难熬的季节，金毛...");
+		map.put("imagJt", R.drawable.rightjt);
+		list.add(map);
+
+		map=new HashMap<String, Object>();
+		map.put("imag", R.drawable.dog2);
+		map.put("title1", "西伯利亚雪橇犬");
+		map.put("title2", "Siberian husky");
+		map.put("title3", "西伯利亚雪橇犬与其他猎犬不一样...");
+		map.put("imagJt", R.drawable.rightjt);
+		list.add(map);
+		
+		map=new HashMap<String, Object>();
+		map.put("imag", R.drawable.dog3);
+		map.put("title1", "萨摩耶犬");
+		map.put("title2", "Samoyed");
+		map.put("title3", "清洗注意事项，基础训练，玩具管理...");
+		map.put("imagJt", R.drawable.rightjt);
+		list.add(map);
+		
+		map=new HashMap<String, Object>();
+		map.put("imag", R.drawable.dog4);
+		map.put("title1", "贵宾犬");
+		map.put("title2", "Poodle");
+		map.put("title3", "日常养护，狗粮益生菌选择，喂养技巧...");
+		map.put("imagJt", R.drawable.rightjt);
+		list.add(map);
+		
+		map=new HashMap<String, Object>();
+		map.put("imag", R.drawable.dog5);
+		map.put("title1", "比雄犬");
+		map.put("title2", "Bichon Frise");
+		map.put("title3", " ");
+		map.put("imagJt", R.drawable.rightjt);
+		list.add(map);
+		return list;
+	}
+
+}
