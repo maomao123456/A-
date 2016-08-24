0d0ba6b60ad52102be278c86f344030ca5e0c4e8
maomao123456 <1396643814@qq.com>
2016-08-24 16:02:03 +0800

    完善第三方 解析第三方数据

--- Pet/src/com/example/pet/qq/QqAppConstant.java
+++ Pet/src/com/example/pet/qq/QqAppConstant.java
@@ -5,9 +5,9 @@ package com.example.pet.qq;
  * @author Administrator
  *
  */
-public class AppConstant {
+public class QqAppConstant {
 	/**
-	 * 介入应用申请的id
+	 * 介入QQ应用申请的id
 	 */
 	 public static String APP_ID="1105564187";
 }
