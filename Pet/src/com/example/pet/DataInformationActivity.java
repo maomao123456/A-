8362f825b015bd54271a6dc5acb1ffdb23d80e1b
maomao123456 <1396643814@qq.com>
2016-09-07 16:10:24 +0800

    第三方用户信息存入数据库

--- Pet/src/com/example/pet/DataInformationActivity.java
+++ Pet/src/com/example/pet/DataInformationActivity.java
@@ -131,7 +131,7 @@ public class DataInformationActivity extends Activity {
 				JSONObject jsonObject = new JSONObject(str);
 				String nickName = jsonObject.getString("nickname");
 				nickname.setText(nickName);
-				String sex = jsonObject.getString("gender");
+				String sex = jsonObject.getString("sex");
 				if (sex.equals("男")) {
 					boy.setChecked(true);
 				} else {
